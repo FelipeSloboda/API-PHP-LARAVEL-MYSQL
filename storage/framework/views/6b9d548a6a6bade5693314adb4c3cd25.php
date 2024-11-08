917af89606aa8068caee00d6147ea499e5a120dd
Felipe Sloboda <felipe.ti.sloboda@gmail.com>
2024-11-08 16:06:34 -0300

    v1

---
+++ storage/framework/views/6b9d548a6a6bade5693314adb4c3cd25.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <H5>welcome</H5>
+</body>
+</html><?php /**PATH C:\laragon\www\api\resources\views/welcome.blade.php ENDPATH**/ ?>
